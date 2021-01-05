d929b63e465ae872bb52f87145c4c50c8dbbd160
GrayHead <ss.zhuravlov@gmail.com>
2021-01-05 15:19:41 +0200

    clock

--- src/App.js
+++ src/App.js
@@ -1,34 +1,58 @@
-import React, {Component, createRef} from 'react';
-import Clock from './Clock';
+import React, {Component} from 'react';
+import ClockComponent from './components/Clock';
 
 class App extends Component {
-
 	state = {
 		clocks: []
 	};
+
+
+	deleteClock = (id) => {
+		let {clocks} = this.state;
+		console.log(clocks);
+		let filterClocks = clocks.filter(clock => clock.id !== id);
+		clocks = filterClocks;
+		this.setState({clocks});
+
+	};
+
 	setClock = (e) => {
 		e.preventDefault();
 		let offset = +e.target[0].value;
-		this.state.clocks.push({offset});
-		this.setState(this.state.clocks)
-
+		console.log(offset);
+		let clock = {
+			id: new Date().getTime(),
+			offset
+		};
+		this.state.clocks.push(clock);
+		this.setState(this.state);
 
 	};
 
 	render() {
+		let {clocks} = this.state;
 		return (
 			<div>
 
 				<form onSubmit={this.setClock}>
-					<input type="number" min={0} max={24}/>
+					<input type="number" min={0}/>
 					<button>set clock</button>
-
-
 				</form>
-				{
-					this.state.clocks.map(clock => <Clock offset={clock.offset}/>)
-				}
+				<div>
+					<h2>world clocks</h2>
+					<div>
+						{
+							clocks.map(clock => <ClockComponent
+								data={clock}
+								key={clock.id}
+								deleteClock={this.deleteClock}
 
+							/>)
+						}
+
+					</div>
+
+				</div>
 
 			</div>
 		);
